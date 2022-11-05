0171a76940a98e3bfe838420bc301b3115a0b15d
Rob <rob.p.tec@gmail.com>
2022-11-05 18:41:29 +0000

    node 14

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16
+FROM node:14
 
 ENV DEBIAN_FRONTEND=noninteractive
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,14 @@ set -e
 
 function setup() {
 
+    FILE=/app/vendure/package.json
+
+    if [ ! -f "$FILE" ]; then
+        echo -e "vendure source code repo not found."
+        echo -e "did you clone it correctly?"
+        exit 2
+    fi
+
     cd /app/vendure/
     
     # steps as per https://github.com/vendure-ecommerce/vendure#development
